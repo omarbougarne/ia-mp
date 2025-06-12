8dc3a315aa370da0298056a92e7afa8bb9bad800
ManaR-Rch <manarmarchou6@gmail.com>
2025-06-12 10:16:38 +0100

    add blog componentCard

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,27 @@ import SignUp from "./Components/SignUp"
 import { BrowserRouter as Router } from "react-router-dom";
 import IAMPCategoriesPage from "./Components/IAMPCategoriesPage"
 import IAMPContactPage from "./Components/IAMPContactPage"
+import IAMPBlogPage from "./Components/IAMPBlogPage"
 
 function App() {
-
-
   return (
     <>
-    <Router>
-    <Routes>
-      {/* Route for the Home page */}
-  
-  <Route path="/IAMPHomepage" element={<IAMPHomepage />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/singin" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/IAMPContactPage" element={<IAMPContactPage />} />
-
-      <Route path="/IAMPCategoriesPage" element={<IAMPCategoriesPage />} />
-      </Routes>
+      <Router>
+        <Routes>
+          {/* Route par défaut pour la page d'accueil */}
+          <Route path="/" element={<IAMPHomepage />} />
+          
+          {/* Autres routes */}
+          <Route path="/IAMPHomepage" element={<IAMPHomepage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/signin" element={<SignIn />} /> {/* Correction de "singin" en "signin" */}
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/IAMPContactPage" element={<IAMPContactPage />} />
+          <Route path="/IAMPCategoriesPage" element={<IAMPCategoriesPage />} />
+          
+          {/* Nouvelle route pour la page de blog */}
+          <Route path="/blog" element={<IAMPBlogPage />} />
+        </Routes>
       </Router>
     </>
   )
